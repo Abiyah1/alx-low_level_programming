9b6e9913f56098cfe44d8aa2c774d4c79d4e9fb1
Abiyah1 <abiyahyahwin@yahoo.com>
2023-02-19 11:03:15 -0800

    dusted

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,20 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A c program that prints the size of data
+ * Return: 0
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float d;
+
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
