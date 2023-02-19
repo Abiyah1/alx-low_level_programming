ee3d74da7f613a2775cee1d7910f3058d93b6391
Abiyah1 <abiyahyahwin@yahoo.com>
2023-02-19 05:04:34 -0800

    getting easy

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A programme that prints a line with printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
